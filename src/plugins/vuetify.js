a5ffd581820138ba4a3690303b096d39a0e00d09
Mariano <mmbatti@gmail.com>
2025-07-10 14:56:15 -0300

    add purgercss

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,6 @@
-// Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
-// Vuetify
 import { createVuetify } from 'vuetify'
 
 import * as components from 'vuetify/components'
@@ -11,4 +9,14 @@ import * as directives from 'vuetify/directives'
 export default createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      mdi: {
+        font: {
+          display: 'swap',
+        },
+      },
+    },
+  },
 })
